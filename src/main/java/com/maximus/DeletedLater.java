5c9499b4742d7e50a53a944b11c807f853673142
m37334 <hsiaolinchu@maximus.com>
2023-07-25 22:07:31 -0700

    add getter and setter

--- src/main/java/com/maximus/DeletedLater.java
+++ src/main/java/com/maximus/DeletedLater.java
@@ -7,4 +7,34 @@ public class DeletedLater {
 	boolean isPushFromLocal;
 	boolean isPushFromLocal3;
 	boolean isUpdateFromRemote;
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	public boolean isTest() {
+		return isTest;
+	}
+	public void setTest(boolean isTest) {
+		this.isTest = isTest;
+	}
+	public boolean isPushFromLocal() {
+		return isPushFromLocal;
+	}
+	public void setPushFromLocal(boolean isPushFromLocal) {
+		this.isPushFromLocal = isPushFromLocal;
+	}
+	public boolean isPushFromLocal3() {
+		return isPushFromLocal3;
+	}
+	public void setPushFromLocal3(boolean isPushFromLocal3) {
+		this.isPushFromLocal3 = isPushFromLocal3;
+	}
+	public boolean isUpdateFromRemote() {
+		return isUpdateFromRemote;
+	}
+	public void setUpdateFromRemote(boolean isUpdateFromRemote) {
+		this.isUpdateFromRemote = isUpdateFromRemote;
+	}
 }

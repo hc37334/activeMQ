1c128a7848caa0fe93c2cc210561b88f645ea63f
hc37334 <hsiaolinchu@maximus.com>
2023-08-04 21:24:39 -0700

    test

--- src/main/java/com/maximus/DeletedLater.java
+++ src/main/java/com/maximus/DeletedLater.java
@@ -3,43 +3,4 @@ package com.maximus;
 public class DeletedLater {
 	
 
-	boolean r1;
-	boolean r3;
-
-
-	public String getTest() {
-		return test;
-	}
-	public void setTest(String test) {
-		this.test = test;
-	}
-	public boolean isTest() {
-		return isTest;
-	}
-	public void setTest(boolean isTest) {
-		this.isTest = isTest;
-	}
-	public boolean isPushFromLocal() {
-		return isPushFromLocal;
-	}
-	public void setPushFromLocal(boolean isPushFromLocal) {
-		this.isPushFromLocal = isPushFromLocal;
-	}
-	public boolean isPushFromLocal3() {
-		return isPushFromLocal3;
-	}
-	public void setPushFromLocal3(boolean isPushFromLocal3) {
-		this.isPushFromLocal3 = isPushFromLocal3;
-	}
-	public boolean isUpdateFromRemote() {
-		return isUpdateFromRemote;
-	}
-	public void setUpdateFromRemote(boolean isUpdateFromRemote) {
-		this.isUpdateFromRemote = isUpdateFromRemote;
-	}
-	@Override
-	public String toString() {
-		return "DeletedLater [test=" + test + ", isTest=" + isTest + ", isPushFromLocal=" + isPushFromLocal
-				+ ", isPushFromLocal3=" + isPushFromLocal3 + ", isUpdateFromRemote=" + isUpdateFromRemote + "]";
-	}
 }

2c41d88f39d7a5ee22a203cf9b6292cb2fc8b627
hc37334 <hsiaolinchu@maximus.com>
2023-08-05 22:36:27 -0700

    delete commit 2

--- src/main/java/com/maximus/DeletedLater.java
+++ src/main/java/com/maximus/DeletedLater.java
@@ -3,44 +3,8 @@ package com.maximus;
 public class DeletedLater {
 	
 
-	string mr1;
-	string mr2;
-	string mr3;
 
-	
-	public String getTest() {
-		return test;
-	}
-	public void setTest(String test) {
-		this.test = test;
-	}
-	public boolean isTest() {
-		return isTest;
-	}
-	public void setTest(boolean isTest) {
-		this.isTest = isTest;
-	}
-	public boolean isPushFromLocal() {
-		return isPushFromLocal;
-	}
-	public void setPushFromLocal(boolean isPushFromLocal) {
-		this.isPushFromLocal = isPushFromLocal;
-	}
-	public boolean isPushFromLocal3() {
-		return isPushFromLocal3;
-	}
-	public void setPushFromLocal3(boolean isPushFromLocal3) {
-		this.isPushFromLocal3 = isPushFromLocal3;
-	}
-	public boolean isUpdateFromRemote() {
-		return isUpdateFromRemote;
-	}
-	public void setUpdateFromRemote(boolean isUpdateFromRemote) {
-		this.isUpdateFromRemote = isUpdateFromRemote;
-	}
-	@Override
-	public String toString() {
-		return "DeletedLater [test=" + test + ", isTest=" + isTest + ", isPushFromLocal=" + isPushFromLocal
-				+ ", isPushFromLocal3=" + isPushFromLocal3 + ", isUpdateFromRemote=" + isUpdateFromRemote + "]";
-	}
+//commit 3
+	//commit 4
+	//commit 5
 }

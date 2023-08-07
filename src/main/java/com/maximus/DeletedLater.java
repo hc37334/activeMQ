50ec90e85c1bfa5a5989683e909b1ea55b6c6570
hc37334 <hsiaolinchu@maximus.com>
2023-08-06 21:38:11 -0700

    c9 reward the commit

--- src/main/java/com/maximus/DeletedLater.java
+++ src/main/java/com/maximus/DeletedLater.java
@@ -7,4 +7,11 @@ public class DeletedLater {
 //commit 3
 	//commit 4
 	//commit 5
+<<<<<<< master
+=======
+	//commit 6
+	//commit 7
+	//commit 8
+	//commit 9
+>>>>>>> b763502 c9 reward the commit
 }
